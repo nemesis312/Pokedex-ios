e2bfe3f917b53e2023b56fa057d55e04c76de196
BoricuaCoder <lesther312@gmail.com>
2024-11-26 09:07:16 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Lesther Ruiz on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Lesther Ruiz on 11/26/24.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
